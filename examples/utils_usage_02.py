c6042c24d1c6d38f55f7ca46834c18e1d6ecb2c0
Konstantin Freidlin <freidlinks@ya.ru>
2022-11-10 01:07:55 +0300

    Add todo

--- examples/utils_usage_02.py
+++ examples/utils_usage_02.py
@@ -20,6 +20,11 @@ if __name__ == '__main__':
     result = u.stt(0)(u.reduce)([1, 2, 3])(0)(u.add)()
     print(result)  # 6
 
-    result = u.stt(0)(u.reducef)([1, 2, 3])(0)(u.add)()  # TypeError: reduce() takes 1 positional argument but 3 were given
-    print(result)  # 6
+    # TypeError: reduce() takes 1 positional argument but 3 were given
+    # result = u.stt(0)(u.reducef)([1, 2, 3])(0)(u.add)()
+    # print(result)  # 6
 
+    # TODO: Hmm...
+    print(u.add(2, 3, 4)())
+    print(fn(1, 2, 3)())
+    print(u.stt(0)(fn)(2, 3)(1)())
